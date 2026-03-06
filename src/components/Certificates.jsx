f33874cda0b041d021b648f01bf872d639bed5ef
Harish SN <snharish85@gmail.com>
2026-03-06 21:24:07 +0530

    cert edited

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -10,18 +10,18 @@ export default function Certificates() {
             img: "/aws-saa.png"
         },
         {
-        title: "HashiCorp Certified: Terraform Associate (004)",
-        issuer: "HashiCorp",
-        date: "2026",
-        link: "https://www.credly.com/earner/earned/badge/e2dee9fd-c02f-40db-83a5-67af7e420994",
-        img: "/terraform-associate.png"
+            title: "HashiCorp Certified: Terraform Associate (004)",
+            issuer: "HashiCorp",
+            date: "2026",
+            link: "https://www.credly.com/earner/earned/badge/e2dee9fd-c02f-40db-83a5-67af7e420994",
+            img: "/terraform-associate.png"
         },
         {
-        title: "Kubernetes and Cloud Native Associate (KCNA)",
-        issuer: "Cloud Native Computing Foundation",
-        date: "2026",
-        link: "https://www.credly.com/earner/earned/badge/547bdfe4-b9a7-42b1-bdcb-3acc2332f7be",
-        img: "/kcna.png"
+            title: "Kubernetes and Cloud Native Associate (KCNA)",
+            issuer: "Cloud Native Computing Foundation",
+            date: "2026",
+            link: "https://www.credly.com/earner/earned/badge/547bdfe4-b9a7-42b1-bdcb-3acc2332f7be",
+            img: "/kcna.png"
        }
     ];
 
